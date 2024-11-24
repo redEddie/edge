bc0154972d151068903c249bfd1a450d87440a0c
redEddie <cpsc.chanwook@gmail.com>
2024-11-24 18:11:18 +0900

    upload

--- lstm/run_lstm.sh
+++ lstm/run_lstm.sh
@@ -6,42 +6,42 @@ REPEAT=12
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem_grow"
-  python3 lstm_eval4.py --gpu --memory_growth True
+  python3 lstm_eval.py --gpu --memory_growth True
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 2048"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 2048
+  python3 lstm_eval.py --gpu --gpu_mem_limit 2048
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 1024"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 1024
+  python3 lstm_eval.py --gpu --gpu_mem_limit 1024
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 512"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 512
+  python3 lstm_eval.py --gpu --gpu_mem_limit 512
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 256"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 256
+  python3 lstm_eval.py --gpu --gpu_mem_limit 256
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 128"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 128
+  python3 lstm_eval.py --gpu --gpu_mem_limit 128
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with mem 64"
-  python3 lstm_eval4.py --gpu --gpu_mem_limit 64
+  python3 lstm_eval.py --gpu --gpu_mem_limit 64
 done
 
--- lstm/run_lstm_cpu.sh
+++ lstm/run_lstm_cpu.sh
@@ -6,23 +6,23 @@ REPEAT=12
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with 4 Cores"
-  python3 lstm_eval4.py --cpu_cores 4
+  python3 lstm_eval.py --cpu_cores 4
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with 3 Cores"
-  python3 lstm_eval4.py --cpu_cores 3
+  python3 lstm_eval.py --cpu_cores 3
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with 2 Cores"
-  python3 lstm_eval4.py --cpu_cores 2
+  python3 lstm_eval.py --cpu_cores 2
 done
 
 for i in $(seq 1 $REPEAT)
 do
   echo ">>> Evaluation $i with 1 Cores"
-  python3 lstm_eval4.py --cpu_cores 1
+  python3 lstm_eval.py --cpu_cores 1
 done
