56d91d10d68bdcc4dd6832910786f00283f71657
redEddie <78861908+redEddie@users.noreply.github.com>
2024-12-10 21:07:42 +0900

    Update lstm_train.py

--- lstm/lstm_train.py
+++ lstm/lstm_train.py
@@ -251,10 +251,10 @@ print(X_test.shape, Y_test.shape)
 model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
 model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
 model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(2, activation="softmax"))
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-print(model.summary())
 
 # ## Heavy LSTM model
 # model = Sequential()
