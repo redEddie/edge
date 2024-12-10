7c4542ef3d69c2c11edba64249968dcee42b96ff
redEddie <78861908+redEddie@users.noreply.github.com>
2024-12-10 21:08:52 +0900

    Update gru_train.py

--- gru/gru_train.py
+++ gru/gru_train.py
@@ -229,43 +229,15 @@ print(X_test.shape, Y_test.shape)
 #####################
 # Define and train the model
 #####################
-## >>> Light GRU
 model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
-model.add(SpatialDropout1D(0.5))
-
-# Add a large GRU layer to slow down inference
-model.add(GRU(32, return_sequences=True))
-model.add(GRU(32, return_sequences=False))
-model.add(Dropout(0.5))
-
-# Optional: BatchNormalization layer to add slight computational overhead
-model.add(BatchNormalization())
-
-# Output layer for binary classification
+model.add(SpatialDropout1D(0.2))
+model.add(GRU(100, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
+model.add(GRU(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(2, activation="softmax"))
-## <<< Light GRU
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-## >>> Heavy GRU
-# model = Sequential()
-# model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
-# model.add(SpatialDropout1D(0.5))
-
-# # Add a large GRU layer to slow down inference
-# # model.add(GRU(128, return_sequences=True))
-# model.add(GRU(64, return_sequences=False))
-# model.add(Dropout(0.5))
-
-# # Optional: BatchNormalization layer to add slight computational overhead
-# model.add(BatchNormalization())
-
-# # Output layer for binary classification
-# model.add(Dense(2, activation="softmax"))
-## <<< Heavy GRU
-
-# Compile the model
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-print(model.summary())
+# print(model.summary())
 
 # Train the model
 epochs = 5
