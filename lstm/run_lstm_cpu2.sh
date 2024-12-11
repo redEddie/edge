f28e8103fd75b65ca398229f474b8c244154e416
redEddie <78861908+redEddie@users.noreply.github.com>
2024-12-11 17:57:28 +0900

    Create run_lstm_cpu2.sh

---
+++ lstm/run_lstm_cpu2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Constants
+REPEAT=12
+CORES=(8 7 6 5 4 3 2 1)
+
+for core in "${CORES[@]}"
+do
+  for i in $(seq 1 $REPEAT)
+  do
+    echo ">>> Evaluation $i with $core Core(s)"
+    python3 lstm_eval.py --cpu_cores $core
+  done
+done
